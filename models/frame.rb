352a938426dd40774aee5236fd016ae92d85c042
lizconlan <lizconlan@gmail.com>
2012-05-30 11:46:10 +0100

    sketching in a bit more detail

--- models/frame.rb
+++ models/frame.rb
@@ -4,6 +4,7 @@ class Frame
   include MongoMapper::Document
   
   key :_type, String
+  key :title, String
   key :timestamp, Time
   key :text, String
 end
--- server.rb
+++ server.rb
@@ -8,20 +8,82 @@ MongoMapper.connect(env)
 
 require_relative 'models/frame'
 
-get "/" do
+helpers do
+  def title_text
+    if params[:title]
+      params[:title]
+    elsif @frame
+      @frame.title
+    else
+      ""
+    end
+  end
+  
+  def frame_text
+    if params[:text]
+      params[:text]
+    elsif @frame
+      @frame.text
+    else
+      ""
+    end
+  end
+  
+  def timestamp_text
+    if params[:ts]
+      params[:ts]
+    elsif @frame
+      @frame.timestamp
+    else
+      Time.now()
+    end
+  end
 end
 
-#editing screen to add new frame
-get "/edit/add" do
+get "/" do
+  @frame = Frame.last
+  haml :index
+end
+
+#admin home screen
+get "/admin/?" do
+  @frames = Frame.all
+  haml :admin
+end
+
+#input screen to add new frame
+get "/admin/new/?" do
+  haml :admin_edit
 end
 
 #submitted frame for validation/saving
-post "/edit/add" do
+post "/admin/new" do
   # check everything's ok...
   
   # ...it is? Great, save the object
-  #frame = Frame.new
-  #frame.timestamp = 
-  #frame.text =
-  #frame.save
+  frame = Frame.new
+  frame.title = params["title"]
+  frame.timestamp = params["ts"]
+  frame.text = params["text"]
+  frame.save
+  
+  #aaand back to the list screen (uh, kinda assuming everything's ok still)
+  redirect "/admin"
+end
+
+#view/edit a frame
+get "/admin/:id/?" do
+  @frame = Frame.find(params[:id])
+  haml :admin_edit
+end
+
+#handle the form submit
+post "/admin/:id/?" do
+  frame = Frame.find(params[:id])
+  frame.title = params["title"]
+  frame.timestamp = params["ts"]
+  frame.text = params["text"]
+  frame.save
+  
+  redirect "/admin"
 end
